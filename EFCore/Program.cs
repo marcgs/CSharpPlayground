f9daa0ee899a18537e8f89a01b0ba7cd89e86513
Marc Gomez <marcgomez@microsoft.com>
2021-02-11 08:46:13 +0100

    Add first Program code accessing the db

--- EFCore/Program.cs
+++ EFCore/Program.cs
@@ -2,11 +2,37 @@
 
 namespace EFCore
 {
+    using System.Linq;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new BloggingContext())
+            {
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog() {Url = "https://marcgs.github.io/"});
+                db.SaveChanges();
+                
+                // Read
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+                
+                // Update
+                Console.WriteLine("Updating the blog and adding a post");
+                blog.Url = "https://marcgomez.ch";
+                blog.Posts.Add(
+                    new Post() { Title = "Hello World", Content = "Hello World!"}
+                );
+                db.SaveChanges();
+                
+                // Delete
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+            }
         }
     }
 }
